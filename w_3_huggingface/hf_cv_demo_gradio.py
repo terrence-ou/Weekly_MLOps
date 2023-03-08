90410ce67d0b363674c91716a1fb0a39515334fa
Tingsong (Terrence) Ou <outerrencedl@gmail.com>
2023-03-07 23:42:17 -0500

    week 3 gradio updated

--- w_3_huggingface/hf_cv_demo_gradio.py
+++ w_3_huggingface/hf_cv_demo_gradio.py
@@ -1,7 +1,20 @@
+import requests
 import gradio as gr
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+from PIL import Image
 
-def greet(name):
-    return "Hello " + name + "!!"
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch(share=True)
+def predict_from_url(url):
+    image = Image.open(requests.get(url, stream=True).raw)
+    feature_extractor = ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
+    model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+    inputs = feature_extractor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_class_idx = logits.argmax(-1).item()
+    result = f"Predicted class: {model.config.id2label[predicted_class_idx]}"
+    return result
+
+if __name__ == "__main__":
+    iface = gr.Interface(fn=predict_from_url, inputs="text", outputs="text")
+    iface.launch()
